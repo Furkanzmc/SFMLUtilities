8b827f235ae75a3701afff7cd9190f4d6885c745
Furkan Üzümcü <furkanuzumcu@gmail.com>
2014-05-26 18:04:06 +0300

    Update SFMLUtilities.cpp
    update the random number generation function to use std::chrono::high_resolution_clock as seed

--- SFMLUtilities.cpp
+++ SFMLUtilities.cpp
@@ -56,11 +56,11 @@ const sf::IntRect SFMLUtilities::getScaledTextureRect(const sf::Sprite &sprite)
     return intRect;
 }
 
-int SFMLUtilities::generateRandomNumber(int start, int end, int seed)
+int SFMLUtilities::generateRandomNumber(int start, int end)
 {
-    std::random_device rd; // obtain a random number from hardware
-    std::mt19937 eng(seed == 0 ? rd() : seed); // seed the generator
-    std::uniform_int_distribution<> distr(start, end); // define the range
+    auto seed = std::chrono::high_resolution_clock::now().time_since_epoch().count();
+    std::mt19937 eng(seed);
+    std::uniform_int_distribution<> distr(start, end);
     return distr(eng);
 }
 
